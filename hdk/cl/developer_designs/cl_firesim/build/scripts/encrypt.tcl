f62b66d973e6e92bda404671487cb65683ba62ea
Sagar Karandikar <sagark@eecs.berkeley.edu>
2017-07-31 13:08:27 +0000

    update cl_firesim with shell v1.3.0 changes from cl_hello_world

--- hdk/cl/developer_designs/cl_firesim/build/scripts/encrypt.tcl
+++ hdk/cl/developer_designs/cl_firesim/build/scripts/encrypt.tcl
@@ -36,13 +36,11 @@ file copy -force $CL_DIR/design/cl_firesim.sv                         $TARGET_DI
 file copy -force $CL_DIR/design/cl_firesim_generated.sv               $TARGET_DIR 
 file copy -force $CL_DIR/../common/design/cl_common_defines.vh        $TARGET_DIR 
 file copy -force $UNUSED_TEMPLATES_DIR/unused_apppf_irq_template.inc  $TARGET_DIR
-file copy -force $UNUSED_TEMPLATES_DIR/unused_aurora_template.inc     $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_cl_sda_template.inc     $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_ddr_a_b_d_template.inc  $TARGET_DIR
 # we will use ddr_c unlike hello world:
 #file copy -force $UNUSED_TEMPLATES_DIR/unused_ddr_c_template.inc      $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_dma_pcis_template.inc   $TARGET_DIR
-file copy -force $UNUSED_TEMPLATES_DIR/unused_hmc_template.inc        $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_pcim_template.inc       $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_sh_bar1_template.inc    $TARGET_DIR
 file copy -force $UNUSED_TEMPLATES_DIR/unused_flr_template.inc        $TARGET_DIR
