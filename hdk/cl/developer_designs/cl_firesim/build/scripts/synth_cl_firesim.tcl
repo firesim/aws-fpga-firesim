6f5738e801923e7450c17f22a4345756297a632e
David Biancolin <david.biancolin@gmail.com>
2017-12-10 00:38:02 +0000

    Revert "Support 16GiB target memory systems"
    This reverts commit d94cae6a3bdcea868e9f9e7a99aa12780f006e9c.

--- hdk/cl/developer_designs/cl_firesim/build/scripts/synth_cl_firesim.tcl
+++ hdk/cl/developer_designs/cl_firesim/build/scripts/synth_cl_firesim.tcl
@@ -60,7 +60,6 @@ read_ip [ list \
   $CL_DIR/ip/axi_clock_converter_dramslim/axi_clock_converter_dramslim.xci \
   $CL_DIR/ip/axi_clock_converter_oclnew/axi_clock_converter_oclnew.xci \
   $CL_DIR/ip/axi_clock_converter_512_wide/axi_clock_converter_512_wide.xci \
-  $CL_DIR/ip/axi_dwidth_converter_0/axi_dwidth_converter_0.xci \
   $CL_DIR/ip/clk_wiz_0_firesim/clk_wiz_0_firesim.xci
 ]
 
