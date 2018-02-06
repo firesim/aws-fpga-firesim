c601ef5ca71f6dd2a588484a32db1b80a3e5e191
Cloud User <dgkim@eecs.berkeley.edu>
2018-02-06 21:25:11 +0000

    donggyu fix
    Conflicts:
    	hdk/cl/developer_designs/cl_firesim/design/cl_firesim.sv

--- hdk/cl/developer_designs/cl_firesim/design/cl_firesim.sv
+++ hdk/cl/developer_designs/cl_firesim/design/cl_firesim.sv
@@ -110,8 +110,9 @@ logic clock_gend_90;
 logic clock_gend_75;
 
 logic firesim_internal_clock;
-assign firesim_internal_clock = clock_gend_75;
- 
+// assign firesim_internal_clock = clock_gend_75;
+assign firesim_internal_clock = clk_extra_a1;
+
 clk_wiz_0_firesim firesim_clocking
 (
     // Clock out ports
